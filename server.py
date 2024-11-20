7c79134743bbe1fdab53d36cd69b9f8bd1526df3
Nicholas Yamamoto <54457924+nyamamoto19@users.noreply.github.com>
2024-11-19 17:32:27 -0800

    created frame of logic flow for server

--- server.py
+++ server.py
@@ -3,17 +3,23 @@ import time
 
 maxPacketSize = 1024 #set arbitrary max packet size to 1024 bits
 serverIP = input("Enter server IP: ") #user inputs an ip
-#serverIP = ipaddress.IPv4Address(IP)
 TCPPort = int(input("Enter port number: ")) #user inputs the port number
 TCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET specifies that it will use ipv4 addresses, SOCK_STREAM specifies that it will use TCP
 TCPSocket.bind((serverIP,TCPPort))  # this will bind the ip and the port to the socket
 TCPSocket.listen(5) #this will allow 5 connection requests to be queued before we accept. If there are 6 requests before we accept, one will get dropped
 incomingSocket, incomingAddress = TCPSocket.accept() #accepts a connection and creates a socket object
 print("Connection from",incomingAddress)
-while True: #will infinitely run
-    packetData = incomingSocket.recv(maxPacketSize) #recieves message from client
-    print("Message received:",packetData.decode())
-    someData = packetData.upper() #changes the recieved message to all uppercase
-    print("Message sending back:", someData.decode())
-    incomingSocket.send(bytearray(str(someData.decode()), encoding="utf-8")) #sends message back to the sender as a byte array with the utf-8 encoding
-    time.sleep(2)
+
+
+query = incomingSocket.recv(maxPacketSize) #recieves query from client
+if query == 1:
+    #What is the average moisture inside my kitchen fridge in the past three hours?
+    pass
+elif query == 2:
+    #What is the average water consumption per cycle in my smart dishwasher?
+    pass
+elif query ==3:
+    #Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?
+    pass
+else:
+    incomingSocket.send(bytearray("Invalid query"), encoding="utf-8")
