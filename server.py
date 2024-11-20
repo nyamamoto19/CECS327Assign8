13fa07401ad7d003f5351e1d1fe66a81adb96c4e
Nicholas Yamamoto <nicholas.yamamoto@gmail.com>
2024-11-19 20:19:43 -0800

    Connected MongoDB to server

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import time
 from pymongo import MongoClient
-
+'''
 maxPacketSize = 1024 #set arbitrary max packet size to 1024 bits
 serverIP = input("Enter server IP: ") #user inputs an ip
 TCPPort = int(input("Enter port number: ")) #user inputs the port number
@@ -10,8 +10,19 @@ TCPSocket.bind((serverIP,TCPPort))  # this will bind the ip and the port to the 
 TCPSocket.listen(5) #this will allow 5 connection requests to be queued before we accept. If there are 6 requests before we accept, one will get dropped
 incomingSocket, incomingAddress = TCPSocket.accept() #accepts a connection and creates a socket object
 print("Connection from",incomingAddress)
+'''
+def get_database():
+    url = "mongodb+srv://user1:user1Password@cluster0.0f9ks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    client = MongoClient(url)
+    return client
+
+mongoDB = get_database()
+result = mongoDB.test.mongodb_virtual.find() #test is within mongoDB, mongodb_virtual is table name
+for res in result:
+    print(res)
 
 
+'''
 query = incomingSocket.recv(maxPacketSize) #recieves query from client
 if query == 1:
     #What is the average moisture inside my kitchen fridge in the past three hours?
@@ -24,3 +35,4 @@ elif query ==3:
     pass
 else:
     incomingSocket.send(bytearray("Invalid query"), encoding="utf-8")
+    '''
